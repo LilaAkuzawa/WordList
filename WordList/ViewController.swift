30dcf5c7439d8b74219faf81c2569701a2e07ebe
LilaAkuzawa <sk8erboi.0522ra@gmail.com>
2020-05-14 08:29:24 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 阿久沢莉良 on 2020/05/14.
+//  Copyright © 2020 Chai Akuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
